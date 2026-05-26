f0332b1f69b46682b24220e14ea6515ec8944833
Saha202045 <107877191+Saha202045@users.noreply.github.com>
2026-05-26 17:29:44 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='finella-v1';
-const ASSETS=['./', './index.html', './manifest.json'];
-self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
-self.addEventListener('fetch', e=>{
-  if(!e.request.url.startsWith(self.location.origin))return;
-  e.respondWith(fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return res}).catch(()=>caches.match(e.request)));
-});
